609fd44d83d0018328a47eaac905cc9eac1bc1ac
MediaMoots <81146974+MediaMoots@users.noreply.github.com>
2024-02-18 15:01:37 +0800

    Update src/TotkRstbGenerator/Commands/RstbCommands.cs
    Co-authored-by: ArchLeaders <80713508+ArchLeaders@users.noreply.github.com>

--- src/TotkRstbGenerator/Commands/RstbCommands.cs
+++ src/TotkRstbGenerator/Commands/RstbCommands.cs
@@ -12,7 +12,7 @@ public class RstbCommands
         """;
 
     [Command("generate", Aliases = ["gen", "calculate", "calc"], Description = "Calculate the RSTB values for a target RomFS folder")]
-    public async Task Generate([Argument] string romfs, string[]? rstbs, [Option("output", ['o'], Description = GEN_OUTPUT_DESC)] string? output, [Option("padding", ['p'], Description = "Adds padding to every generated RSTB value")] uint padding = 0)
+    public async Task Generate([Argument] string romfs, [Option("output", ['o'], Description = GEN_OUTPUT_DESC)] string? output, [Option("padding", ['p'], Description = "Adds padding to every generated RSTB value")] uint padding = 0)
     {
         RstbGenerator generator = new(romfs, null, output, padding);
         await generator.GenerateAsync();
